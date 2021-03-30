ae7dc9d2a645a6989867441134728512e4277f23
Lory999555 <lorenzomorelli95@gmail.com>
2021-03-30 02:15:15 -0700

    first example of CryptoPP usage

--- src/Modules/IEEE802154Mac.cc
+++ src/Modules/IEEE802154Mac.cc
@@ -22,6 +22,14 @@
 #include "cryptopp/ccm.h"
 #include "cryptopp/aes.h"
 
+#include <iostream>
+#include <iomanip>
+
+#include "cryptopp/modes.h"
+#include "cryptopp/filters.h"
+
+using namespace CryptoPP;
+
 
 Define_Module(IEEE802154Mac);
 
@@ -4559,10 +4567,66 @@ void IEEE802154Mac::handleBcnTxTimer()
             //inserimento Mic
             char tmp[]="FFFF";
 
-            CryptoPP::SecByteBlock key( CryptoPP::AES::DEFAULT_KEYLENGTH );
-          //  AutoSeededRandomPool prng;
-            CryptoPP::byte iv[ 12 ];
-          //  prng.GenerateBlock( iv, sizeof(iv) )
+
+            //
+            // Key and IV setup
+            //
+            byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
+            memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
+            memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+
+            //
+            // String and Sink setup
+            //
+            std::string plaintext = "Now is the time for all good men to come to the aide...";
+            std::string ciphertext;
+            std::string decryptedtext;
+
+            //
+            // Dump Plain Text
+            //
+            std::cout << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
+            std::cout << plaintext;
+            std::cout << std::endl << std::endl;
+
+            //
+            // Create Cipher Text
+            //
+            CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+            CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
+
+            CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
+            stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1 );
+            stfEncryptor.MessageEnd();
+
+            //
+            // Dump Cipher Text
+            //
+            std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
+
+            for( int i = 0; i < ciphertext.size(); i++ ) {
+
+                std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
+            }
+
+            std::cout << std::endl << std::endl;
+
+            //
+            // Decrypt
+            //
+            CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+            CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
+
+            CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
+            stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
+            stfDecryptor.MessageEnd();
+
+            //
+            // Dump Decrypted Text
+            //
+            std::cout << "Decrypted Text: " << std::endl;
+            std::cout << decryptedtext;
+            std::cout << std::endl << std::endl;
 
             // { 4, 6, 8, 10, 12, 14, 16 }
             const int TAG_SIZE = 8;
